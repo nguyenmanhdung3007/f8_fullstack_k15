5bdee4ad89ac0c7ac5cbe31797500e5c2a5b14b6
nguyenmanhdung3007 <nguyenmanhdung3007@gmail.com>
2025-07-09 21:51:37 +0700

    nop bai tap day 12

---
+++ day_12_multiple-page/vite.config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from "vite";
+import { ViteEjsPlugin } from "vite-plugin-ejs";
+import path from "path";
+
+export default defineConfig({
+  base: "/day_12_multiple-page",
+  root: "src",
+  publicDir: "../public",
+  build: {
+    outDir: "../dist",
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, "src/index.html"),
+        about: path.resolve(__dirname, "src/about.html"),
+      },
+    },
+  },
+  plugins: [ViteEjsPlugin()],
+});
